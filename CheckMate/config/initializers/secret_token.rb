2bdeea39d6df66d99449621afeecac4e2e4c4e23
Joel <jnfeinstein@gmail.com>
2012-07-14 10:47:22 -0700

    Added devise authentication

---
+++ CheckMate/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckMate::Application.config.secret_token = 'f9b61eecdf90e9691e8ed7b9c5de03797c623fb19c8fd8fa35c36b0a63f7bda6bb080238d0bd6a9c79af14fc201b8ea43179e845923aa13ecf134406f9d53fbf'
