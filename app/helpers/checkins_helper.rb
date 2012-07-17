fdef2fa8ca92ac78791a0628f8df6a0b16796ddb
Joel <jnfeinstein@gmail.com>
2012-07-17 00:19:47 -0700

    Checkin script works

--- app/helpers/checkins_helper.rb
+++ app/helpers/checkins_helper.rb
@@ -8,30 +8,39 @@ module CheckinsHelper
   @@URL_RETRIEVE = "/flight/retrieveCheckinDoc.html"
   
   def do_checkin(checkin)
-    url = URI.parse("#{@@PROTCOL}#{@@HOST}#{@@URL_RETRIEVE}")
+    url = URI.parse("#{@@PROTOCOL}#{@@HOST}#{@@URL_RETRIEVE}")
     res = Net::HTTP.post_form(url,{
       "formToken" => "",
       "confirmationNumber" => checkin.conf,
       "firstName" => checkin.first,
       "lastName" => checkin.last,
-      "submitButton" => "Check+In"})     
-    if !(res.kindof? Net::HTTPRedirection)
+      "submitButton" => "Check In"})   
+    case res
+    when Net::HTTPRedirection then
+    else
+      puts "Step 1: Not a 302"
       return
     end
-    next_url = res['location']
-    url = URI.parse("#{@@PROTOCOL}#{@@HOST}#{next_url}")
-    res = Net::HTTP.post_form(url,{
+    url = URI.parse(res['location'])
+    req = Net::HTTP::Post.new(url.request_uri)
+    req.set_form_data({
       "formToken" => "",
-      "checkinPassengers.selected" => "true",
-      "printDocuments" => "Check+In"})
-    if !(res.kindof? Net::HTTPRedirection)
+      "checkinPassengers[0].selected" => "true",
+      "printDocuments" => "Check In"},'&')
+    res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
+    case res
+    when !Net::HTTPRedirection then 
+      puts "Step 2: Not a 302"
       return
     end
-    next_url = res['location']
-    url = URI.parse("#{@@PROTOCOL}#{@@HOST}#{next_url}")
+    url = URI.parse(res['location'])
     res = Net::HTTP.get_response(url)
-    if !(res.kind_of? Net::HTTPSuccess)
+    case res
+    when Net::HTTPSuccess then 
+    else
+      puts "Step 3: Not a 200"
       return
     end
+    puts res
   end
 end
